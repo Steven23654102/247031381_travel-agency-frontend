2fbe2bb78a0019d50dab36d3f1b94fb53e308ed8
Steven23654102 <apple23654102@gmail.com>
2025-06-18 16:27:24 +0800

    feat(frontend): add HotelList component and basic layout

--- src/components/HotelList.tsx
+++ src/components/HotelList.tsx
@@ -3,31 +3,34 @@ import axios from 'axios';
 
 interface Hotel {
   name: string;
-  city: string;
-  price: number;
-  image: string;
+  category: string;
+  destination: string;
 }
 
 const HotelList: React.FC = () => {
   const [hotels, setHotels] = useState<Hotel[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/hotels')
-      .then(res => setHotels(res.data))
-      .catch(err => console.error(err));
+    axios.get('http://localhost:3000/api/hotels/hotelbeds?city=PAR')
+      .then(res => setHotels(res.data.hotels))
+      .catch(err => console.error('Error fetching hotels:', err));
   }, []);
 
   return (
     <div>
-      <h2>MongoDB 飯店列表</h2>
-      {hotels.map((hotel, index) => (
-        <div key={index} style={{ border: '1px solid #ccc', marginBottom: 10, padding: 10 }}>
-          <h3>{hotel.name}</h3>
-          <p>城市：{hotel.city}</p>
-          <p>價格：${hotel.price}</p>
-          {hotel.image && <img src={hotel.image} alt={hotel.name} width="200" />}
-        </div>
-      ))}
+      {hotels.length === 0 ? (
+        <p>載入中...</p>
+      ) : (
+        <ul>
+          {hotels.map((hotel, index) => (
+            <li key={index}>
+              <strong>{hotel.name}</strong><br />
+              類型：{hotel.category}<br />
+              城市代碼：{hotel.destination}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
